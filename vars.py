cbdabc6033c18babc6c3057cee79277d67cb6929
AntiHacking <siddharthkumar805100@gmail.com>
2025-10-27 17:45:54 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,7 @@ API_HASH = environ.get("API_HASH", "725f60b51e233703576199334610011a")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8495221315:AAF9Q6qXOYiaKWQExCQU1U6Qwry75hF0jqI")
 
 OWNER = int(environ.get("OWNER", "5666930893"))
-CREDIT = environ.get("CREDIT", "KANHA 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "User")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5666930893').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
